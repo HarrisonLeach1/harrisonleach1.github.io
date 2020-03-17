3ff650020e39293bef05fe3208810275087fd787
Harrison Leach <HarrisonL7190@gmail.com>
2020-03-17 22:36:41 +1300

    Add new projects to project section

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import { Header, Icon, Card, Image, Label, Button } from 'semantic-ui-react';
 import { Parallax } from 'react-parallax';
+import {DiAws, DiJava, DiUnitySmall, DiScriptcs} from "react-icons/di";
 
 class Projects extends Component {
   render() {
@@ -21,15 +22,39 @@ class Projects extends Component {
                 <Icon name='github'/> GitHub
               </Button>
             <div className = 'cardBox'>
+            <Card fluid>
+                <Image fluid src= {require('../../Assets/images/sunny-town.png')}/>
+                <Card.Content>
+                  <Card.Header>Sunny Town - Web Game</Card.Header>
+                  <Card.Meta>October 2019</Card.Meta>
+                  <Card.Description>A Web-based serious game aimed at promoting Climate Change awareness. Designed for children in intermediate school years (aged 12-13). Built with <b>Unity</b> and <b>C#</b> Hosted on <b>AWS S3</b>.
+                  </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                  <Label><DiScriptcs style={{color: "purple"}}/>C#</Label>
+                  <Label><DiUnitySmall/>Unity</Label>
+                  <Label><DiAws style={{color: "orange"}}/>AWS</Label>
+                  </Card.Content>
+              </Card>
+              <Card fluid>
+                <Image fluid src= {require('../../Assets/images/task-scheduler.png')}/>
+                <Card.Content>
+                  <Card.Header>Task Scheduler</Card.Header>
+                  <Card.Meta>August 2019</Card.Meta>
+                  <Card.Description>This project involved using artificial intelligence (AI) and parallel processing power to solve an NP-Hard 
+                                    scheduling problem. Built with <b>Java</b>. Our team achieved first in class for fastest sequential execution.
+                  </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                  <Label><DiJava style={{color: "orangered"}}/>Java</Label>
+                </Card.Content>
+              </Card>
               <Card fluid>
                 <Image fluid src= {require('../../Assets/images/website.PNG')}/>
                 <Card.Content>
                   <Card.Header>This Website</Card.Header>
                   <Card.Meta>July 2018</Card.Meta>
-                  <Card.Description>This website was built using <b>React</b> and the <a href = "https://react.semantic-ui.com/introduction" >
-                  Semantic UI React </a> component library. Project packages and dependencies were managed using <b>Node</b>.  Sass was
-                  used for <b>CSS</b> pre-processing. <b>Gulp</b> was used for task automation including <b>SCSS</b> to <b>CSS</b> compilation
-                  , <b>CSS</b> uglifying and minifying. The website is hosted on GitHub pages.
+                  <Card.Description>A Simple static website built with <b>React</b>, <b>Sass</b> and <b>Gulp</b>. Hosted on GitHub pages and is kind of mobile-friendly.
                   </Card.Description>
                 </Card.Content>
                 <Card.Content extra>
@@ -38,7 +63,6 @@ class Projects extends Component {
                   <Label><Icon name ='css3 alternate' color = 'blue'/>SCSS / CSS</Label>
                   <Label><Icon name ='js' color = 'yellow'/>JavaScript</Label>
                   <a href = "https://gulpjs.com/"><Label><Icon name ='gulp' color = 'red'/>Gulp</Label></a>
-                  <Label><Icon name ='node' color = 'grey'/>Node</Label>
                 </Card.Content>
               </Card>
             </div>
